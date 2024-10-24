2aa3f2be8c2c1dcf1947feef0372f8d529e66d75
Prashant <goden1029@gmail.com>
2024-10-24 15:12:39 +0530

    add apedia

--- Routes/index.js
+++ Routes/index.js
@@ -7,11 +7,12 @@ router.get("/", (req, res) => {
 });
 
 router.post("/sendOtp", (req, res) => {
-  const { otp, companyEmail } = req.body; // Make sure to destructure otp and companyEmail
+  const { otp, companyEmail } = req.body;
+  // Make sure to destructure otp and companyEmail
+  console.log(otp, companyEmail, "herer");
 
-  // Use worker to send email
   const emailWorker = new Worker("./config/nodemailer.js", {
-    workerData: { email: companyEmail, otp }, // Send only otp and email
+    workerData: { email: companyEmail, otp },
   });
 
   // Handle worker messages
@@ -22,6 +23,37 @@ router.post("/sendOtp", (req, res) => {
       res
         .status(500)
         .json({ error: "Failed to send OTP", details: message.error });
+    }
+  });
+
+  // Handle worker errors
+  emailWorker.on("error", (error) => {
+    console.error("Error from worker:", error);
+    res.status(500).json({ error: "Worker encountered an error." });
+  });
+
+  // Handle worker exit
+  emailWorker.on("exit", (code) => {
+    if (code !== 0) {
+      console.error(`Worker stopped with exit code ${code}`);
+    }
+  });
+});
+
+router.post("/reset-link", (req, res) => {
+  const { email, resetToken } = req.body;
+  console.log("reset link");
+  const emailWorker = new Worker("./config/nodemailer2.js", {
+    workerData: { email, resetToken },
+  });
+  // Handle worker messages
+  emailWorker.on("message", (message) => {
+    if (message.success) {
+      res.json({ message: "Reset Link sent successfully to  email." });
+    } else {
+      res
+        .status(500)
+        .json({ error: "Failed to send reset Link", details: message.error });
     }
   });
 
